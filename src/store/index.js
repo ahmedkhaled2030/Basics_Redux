aef730919ca565f73a7d23c9fa5fcc21ac30b91e
codeBindHex <xhunter19192020@gmail.com>
2022-09-10 14:18:04 +0200

    FirstCommit

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,49 @@
-import { createStore } from 'redux';
+
+//**************************old Redux****************************
+// import { createStore } from 'redux';
 
 
-const initState = { value: 0 , showCounter : true };
+// const initState = { value: 0 , showCounter : true };
 
-const counterReducer = (state = initState, action) => {
-    if (action.type === "increase") {
-        return {...state ,value :  state.value + action.payload}
-    }
+// const counterReducer = (state = initState, action) => {
+//     if (action.type === "increase") {
+//         return {...state ,value :  state.value + action.payload}
+//     }
     
-    if (action.type === "decrease") {
-        return {...state ,value : state.value  - action.payload }
-    }
+//     if (action.type === "decrease") {
+//         return {...state ,value : state.value  - action.payload }
+//     }
 
-    if (action.type === 'toggleCounter') {
-        return {...state ,  showCounter : !state.showCounter }
-    }
-    return state;
-}
+//     if (action.type === 'toggleCounter') {
+//         return {...state ,  showCounter : !state.showCounter }
+//     }
+//     return state;
+// }
 
-const store = createStore(counterReducer);
+// const store = createStore(counterReducer);
+
+//**************************old Redux****************************
+
+
+//**************************Redux ToolKit****************************
+import {createSlice , createStore } from '@reduxjs/toolkit';
+
+const initState = { value: 0 };
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: initState,
+    reducers: {
+        increase: (state, action) => {
+            state.value += 1;
+        },
+        decrease: (state, action) => {
+            state.value -= 1;
+        }
+    }
+})
+
+
+const store  = createStore(counterSlice.reducer)
 
 export default store; 
